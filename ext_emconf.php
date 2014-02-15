1b4ba1c9d915f53fd3ab0d7217b8be203be84349
typo3 <typo3@ubuntutestserver.fritz.box>
2014-02-15 08:34:04 +0100

    helloworldbeispiel

---
+++ ext_emconf.php
@@ -0,0 +1,55 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "helloworldbeispiel".
+ *
+ * Auto generated 15-02-2014 07:08
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'HelloWorld',
+	'description' => 'Provider extension for pages content ',
+	'category' => 'misc',
+	'shy' => 0,
+	'version' => '0.0.1',
+	'dependencies' => 'cms,extbase,fluid,flux,fluidpages,fluidcontent,vhs',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'experimental',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 1,
+	'lockType' => '',
+	'author' => 'Christian',
+	'author_email' => 'info@webnugget.de',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '4.5-6.2.99',
+			'cms' => '',
+			'extbase' => '',
+			'fluid' => '',
+			'flux' => '',
+			
+			'fluidpages' => '',
+			'fluidcontent' => '',
+			'vhs' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:0:{}',
+	'suggests' => array(
+	),
+);
